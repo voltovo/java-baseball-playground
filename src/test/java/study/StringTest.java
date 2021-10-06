1b177dc89885df631102327b53e2eb4b415991dd
sumanChoi <voltovo@naver.com>
2021-10-06 21:28:47 +0900

    feat:String class junit Test  practice

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -10,4 +10,23 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    void splitOne() {
+        String[] values = "1,2".split(",");
+        // containsExactly : 순서 포함해서 정확하게 일치
+        assertThat(values).containsExactly("1", "2");
+    }
+
+    @Test
+    void splitTwo() {
+        String[] values = "1".split(",");
+        assertThat(values).contains("1");
+    }
+
+    @Test
+    void splitThree() {
+        String value = "(1,2)".substring(1, 4);
+        assertThat(value).isEqualTo("1,2");
+    }
 }
