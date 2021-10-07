852fbadb0ff52b80b1eb178273b63bf870777660
sumanChoi <voltovo@naver.com>
2021-10-07 21:11:17 +0900

    feat:String class junit Test practice Two

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,11 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class StringTest {
     @Test
@@ -15,18 +18,49 @@ public class StringTest {
     void splitOne() {
         String[] values = "1,2".split(",");
         // containsExactly : 순서 포함해서 정확하게 일치
-        assertThat(values).containsExactly("1", "2");
+        assertThat(values).contains("1", "2");
     }
 
     @Test
     void splitTwo() {
         String[] values = "1".split(",");
-        assertThat(values).contains("1");
+        assertThat(values).containsExactly("1");
     }
 
     @Test
     void splitThree() {
         String value = "(1,2)".substring(1, 4);
         assertThat(value).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("특정 위치 문자 가져오기1")
+    void charGetFromString1() {
+        String text = "abc";
+        int index = 1;
+
+        char value = text.charAt(index);
+        assertThat(value).isEqualTo('b');
+
+    }
+
+    @Test
+    @DisplayName("특정 위치 문자 가져오기2")
+    void charGetFromString2() {
+        String text = "abc";
+        int index = 3;
+        assertThatThrownBy(() -> {
+            text.charAt(index);
+        }).isInstanceOf(IndexOutOfBoundsException.class).hasMessageContaining("%d", index);
+    }
+
+    @Test
+    @DisplayName("특정 위치 문자 가져오기3")
+    void charGetFromString3() {
+        String text = "abc";
+        int index = 3;
+        assertThatExceptionOfType(IndexOutOfBoundsException.class).isThrownBy(() -> {
+            text.charAt(index);
+        }).withMessageMatching("String index out of range: \\d");
     }
 }
