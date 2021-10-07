c9ff6ae3ee5d902aff7368514006abd754a179bc
sumanChoi <voltovo@naver.com>
2021-10-07 23:13:56 +0900

    feat:Set junit Test practice One

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,28 @@
+package study;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    };
+
+    // Test Case 구현
+    @Test
+    void setCheckSize() {
+        assertThat(numbers).hasSize(3);
+    }
+
+}
