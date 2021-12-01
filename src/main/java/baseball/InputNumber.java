582a24a2b4a86cf3ad7c3cecc6280b0a46e17b35
sumanChoi <voltovo@naver.com>
2021-12-01 21:34:59 +0900

    feat: 1 ~ 9 인가 테스트

---
+++ src/main/java/baseball/InputNumber.java
@@ -0,0 +1,14 @@
+package baseball;
+
+public class InputNumber {
+
+    public static boolean validNo(int num) {
+        if (num > 0 && num < 10) {
+
+            
+            return true;
+        }
+        return false;
+    }
+
+}
--- src/test/java/baseball/InputNumberTest.java
+++ src/test/java/baseball/InputNumberTest.java
@@ -1,10 +1,15 @@
 package baseball;
 
 import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class InputNumberTest {
     @Test
     void inputNumberValidation() {
-
+        assertThat(InputNumber.validNo(9)).isTrue();
+        assertThat(InputNumber.validNo(1)).isTrue();
+        assertThat(InputNumber.validNo(0)).isFalse();
+        assertThat(InputNumber.validNo(10)).isFalse(); 
     }
+
 }
